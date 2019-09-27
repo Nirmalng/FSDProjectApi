8f545f7ad2a6d6b4a4c649b7e9679a5dcbac9e97
Admin <Admin@DOTNET>
2019-09-27 09:03:23 -0700

    change1

--- ProjectManager.API/Program.cs
+++ ProjectManager.API/Program.cs
@@ -17,7 +17,6 @@ namespace ProjectManager.API
             CreateWebHostBuilder(args).Build().Run();
         }
 
-
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
